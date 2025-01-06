3ac0273a082de610fb61e134c7844ab6e40969d3
Oscar Moore <oscarjmoore38@gmail.com>
2025-01-06 14:19:57 +0000

    Skeleton for app and testing + build files

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace ::std;
+
+
+int main() {
+    cout << "Hello, GitHub Codespaces! I'm learning how to build C++ projects! ChatGPT is like a drunk uncle" << endl;
+    return 0;
+}
